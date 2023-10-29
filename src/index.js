07d01c91b6f78d8228198575e27e66481b87698f
João Goulart <goulart.jp@hotmail.com>
2023-10-29 14:34:30 -0300

    Added CORS permissions to port 4200

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ const app = express();
 
 sequelize.sync().then(() => console.log('DB is connected'));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/projects', ProjectRoutes);
